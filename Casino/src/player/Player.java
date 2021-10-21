e95c2b2b6e983d0e5f785b46ed6d50b18b342368
HansTH <hans.novi@icloud.com>
2021-10-21 18:07:57 +0200

    add fields in Player class

--- Casino/src/player/Player.java
+++ Casino/src/player/Player.java
@@ -3,16 +3,40 @@ package player;
 public class Player {
 
     private String name;
+    private int coinsToPlay;
+    private int coinsPlayerWon;
 
     public Player(String name) {
         this.name = name;
+        this.coinsToPlay = 1000;
+        this.coinsPlayerWon = 0;
     }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
+    }
+    
+    
+    public int getCoinsToPlay() {
+        return coinsToPlay;
+    }
+    public void setCoinsToPlay(int gamePrice) {
+        this.coinsToPlay -= gamePrice;
+    }
+    
+    
+    public int getCoinsPlayerWon() {
+        return coinsPlayerWon;
+    }
+    public void setCoinsPlayerWon(int coinsPlayerWon) {
+        this.coinsPlayerWon += coinsPlayerWon;
+    }
+    
+    
+    public void addCoinsToPlay() {
+        this.coinsToPlay += coinsPlayerWon;
     }
 }
