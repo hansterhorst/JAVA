41f9069076e761c2c987a6f9025f7dc3288db430
HansTH <hans.novi@icloud.com>
2021-10-21 18:24:31 +0200

    refactor and add functions

--- Casino/src/CasinoMain.java
+++ Casino/src/CasinoMain.java
@@ -1,15 +1,5 @@
-//import blackjack_v1.BlackjackGame;
-//import player.Player;
-
-//import blackjack_v2.blackjack.Blackjack;
-//import blackjack_v2.blackjack.LowBlackjack;
-//import blackjack_v2.blackjack.SimpleBlackjack;
-//import blackjack_v2.deck.LowDeck;
-//import blackjack_v2.deck.SimpleDeck;
-
-//import slotmachineGame.SlotmachineGame;
-
-import hangman.HangmanGame;
+import higherLowerGame.HigherLowerGame;
+import player.Player;
 
 import java.util.Scanner;
 
@@ -21,40 +11,10 @@ public class CasinoMain {
         System.out.println("Welcome to Casino Royal!\n");
         System.out.print("What's your name? ");
         String name = inputScanner.nextLine();
-
-//        Player player = new Player(name);
-
-//        HigherLowerGame higherLowerGame = new HigherLowerGame(inputScanner);
-//        higherLowerGame.playGame(player);
-
-//        BlackjackGame blackjackGame = new BlackjackGame(inputScanner, player);
-//        blackjackGame.playGame();
+        Player player = new Player(name);
     
-//        boolean isTrue = true;
-//        do {
-//            System.out.println(name +
-//                    ", which Blackjack game do you want to play?\n" +
-//                    "1 = Low Blackjack, you will play with cards from two too six.\n" +
-//                    "2 = normal Blackjack.");
-//            String input = inputScanner.nextLine();
-//
-//            if (input.equals("1")) {
-//                Blackjack lowBlackjack = new LowBlackjack(inputScanner, name, new LowDeck());
-//                lowBlackjack.playGame();
-//                isTrue = false;
-//            } else if (input.equals("2")) {
-//                Blackjack blackjack = new SimpleBlackjack(inputScanner, name, new SimpleDeck());
-//                blackjack.playGame();
-//                isTrue = false;
-//            } else {
-//                System.out.println("Wrong input! Try again.");
-//            }
-//        } while (isTrue);
-    
-//        SlotmachineGame game = new SlotmachineGame(inputScanner, name);
-//        game.playGame();
-    
-        HangmanGame game = new HangmanGame(name, inputScanner);
+        HigherLowerGame game = new HigherLowerGame(inputScanner, player);
         game.playGame();
+
     }
 }
--- Casino/src/higherLowerGame/HigherLowerGame.java
+++ Casino/src/higherLowerGame/HigherLowerGame.java
@@ -6,74 +6,129 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HigherLowerGame {
-
-    private final Scanner inputScanner;
-    private final int randomNumber;
-    private int guessedNumber;
-    private int roundCount;
-
-    public HigherLowerGame(Scanner inputScanner) {
-        this.inputScanner = inputScanner;
-        this.randomNumber = new Random().nextInt(99) + 1;
-        this.roundCount = 0;
-    }
-
-    public void playGame(Player player) {
-
-        System.out.println(randomNumber);
-
-        System.out.println("Hello, " + player.getName() + ". Welcome to Higher Lower");
-
-        System.out.println("Guess a number from 1 t/m 100");
-        setGuessedNumber(inputScanner.nextInt());
-
-        while (getGuessedNumber() != getRandomNumber() || getGuessedNumber() == getRandomNumber()) {
+   
+   private final Scanner inputScanner;
+   private final Player player;
+   private int randomNumber;
+   private int guessedNumber;
+   private int roundCount;
+   private boolean isGameRunning;
+   
+   public HigherLowerGame(Scanner inputScanner, Player player) {
+      this.inputScanner = inputScanner;
+      this.player = player;
+   }
+   
+   public void playGame() {
+      this.isGameRunning = true;
+      final int COINS_TO_WIN = 10;
+      final int GAME_PRICE = 1;
+      
+      
+      System.out.println("Hello, " + player.getName() + ". Welcome to Higher Lower");
+      System.out.println("If you win! The game will pay you " + COINS_TO_WIN +
+              " coins minus the rounds you guessed the number.");
+      System.out.println("You have " + player.getCoinsToPlay() + " coins to play with.");
+      
+      while (isGameRunning) {
+         this.randomNumber = new Random().nextInt(99) + 1;
+         this.roundCount = 0;
+         
+         System.out.println("Guess a number from 1 too 100");
+         setGuessedNumber(inputScanner.nextInt());
+         
+         player.setCoinsToPlay(GAME_PRICE);
+         System.out.println(randomNumber);
+         
+         boolean isTrue = true;
+         do {
             if (getGuessedNumber() < getRandomNumber()) {
-                System.out.println("That number is to low!");
-                setRoundCount();
-                newGuess();
+               System.out.println("That number is to low!");
+               setRoundCount();
+               newGuess();
             }
-
             if (getGuessedNumber() > getRandomNumber()) {
-                System.out.println("That number is too high!");
-                setRoundCount();
-                newGuess();
+               System.out.println("That number is too high!");
+               setRoundCount();
+               newGuess();
             }
-
+            
             if (getGuessedNumber() == getRandomNumber()) {
-                roundCount += 1;
-                System.out.println("Correct! " + player.getName() + ". You guessed the number in " + getRoundCount() + " rounds");
-                break;
+               // if player guessed the number in the first round, return 0 else round count
+               int coinsWon = COINS_TO_WIN - (roundCount == 0 ? 0 : roundCount);
+               player.setCoinsPlayerWon(coinsWon);
+               System.out.println("Correct! " + player.getName() +
+                       ". You guessed the number in " + getRoundCount() + " rounds");
+               System.out.println("Your winning coins are " + coinsWon);
+               isTrue = false;
             }
-        }
-
-    }
-
-    private void newGuess() {
-        System.out.print("Make a guess to try again. ");
-        setGuessedNumber(inputScanner.nextInt());
-    }
-
-    public int getRoundCount() {
-        return roundCount;
-    }
-
-    public int getGuessedNumber() {
-        return guessedNumber;
-    }
-
-    public int getRandomNumber() {
-        return randomNumber;
-    }
-
-
-    public void setGuessedNumber(int guessedNumber) {
-        this.guessedNumber = guessedNumber;
-    }
-
-    public void setRoundCount() {
-        this.roundCount += 1;
-    }
-
-
+            
+         } while (isTrue);
+         
+         playAgain();
+      }
+      
+      System.out.println(player.getName() + ", thank you for playing. Your won " +
+              player.getCoinsPlayerWon() + " coins, and your total playing coins are now " +
+              player.getCoinsToPlay());
+   }
+   
+   private void newGuess() {
+      boolean isTrue = true;
+      do {
+         System.out.print("Make a guess to try again. ");
+         String input = inputScanner.next();
+         if (input.matches("[0-9]+")) {
+            setGuessedNumber(Integer.parseInt(input));
+            isTrue = false;
+         } else {
+            System.out.println("Wrong input! Try again.");
+         }
+      } while (isTrue);
+   }
+   
+   private void playAgain() {
+      
+      boolean isTrue = true;
+      while (isTrue) {
+         
+         System.out.println("do you want to play again?\n" +
+                 "1 = Play, 2 = Quit.");
+         String input = inputScanner.next();
+         
+         if (input.equals("1")) {
+            isTrue = false;
+         } else if (input.equals("2")) {
+            player.addCoinsToPlay();
+            isGameRunning = false;
+            isTrue = false;
+         } else {
+            System.out.println("Wrong input! Try again.");
+         }
+         
+      }
+   }
+   
+   public int getRoundCount() {
+      return roundCount;
+   }
+   
+   public int getGuessedNumber() {
+      return guessedNumber;
+   }
+   
+   public int getRandomNumber() {
+      return randomNumber;
+   }
+   
+   
+   public void setGuessedNumber(int guessedNumber) {
+      this.guessedNumber = guessedNumber;
+   }
+   
+   public void setRoundCount() {
+      this.roundCount += 1;
+   }
+   
+   
 }
